6b326f91e84fd45942a232a5a713c87f5bb61539
SharonMawiaJohn <sharonmawia76@gmail.com>
2025-01-27 23:46:34 +0300

    changes made on admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+class CustommUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'city', 'state',),
+        })
+    )
