b4bfe382df1bf431a1ac409820acf336a7510c0a
SharonMawiaJohn <sharonmawia76@gmail.com>
2025-01-27 20:54:08 +0300

    Adds a class in models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    city = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    address = models.CharField(blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
