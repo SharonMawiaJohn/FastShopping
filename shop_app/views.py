413d050a47dbe132d67e3b7a7085bdbb408cad87
SharonMawiaJohn <sharonmawia76@gmail.com>
2025-02-08 23:44:42 +0300

    product in cart handling under urls.py

--- shop_app/views.py
+++ shop_app/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from .models import Product, Cart, CartItem
 from .serializers import DetailedProductSerializer, ProductSerializer, CartItemSerializer
 from rest_framework.response import Response
-
+from rest_framework.decorators import api_view
 # Create your views here.
 
 @api_view(["GET"])
