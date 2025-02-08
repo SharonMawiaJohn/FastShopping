88d7575e238a10f48d7d5903cbde93ff46c71849
SharonMawiaJohn <sharonmawia76@gmail.com>
2025-02-08 13:25:00 +0300

    product in cart handling under urls.py

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path("products", views.products, name="products"),
     path("product_detail/<slug:slug>", views.product_detail, name="product_detail"),
-    path("add_item/", views.add_item, name = "add_item")
+    path("add_item/", views.add_item, name = "add_item"),
+    path("product_in_cart", views.product_in_cart,name="product_in_cart")
 ]
 
 # fetching all_products: http://127.0.0.1:8001/products
